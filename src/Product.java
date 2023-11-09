24aadc038f97bd3956c1bb9fad2ec3257a71f9a9
qwerty1432 <88243251+qwerty1432@users.noreply.github.com>
2023-11-08 21:56:17 -0500

    Made product class

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,33 @@
+package src;
+
 public class Product {
+
+    private int sellerid;
+    private int quantity;
+    private String name;
+    private int[] storeIDs;
+    private String description;
+    private double price;
+    private Seller seller;
+
+    Customer cls = new Customer();
+
+    public Product(int sellerid, int quantity, String name, int[] storeIDs, String description, double price) {
+        this.sellerid = sellerid;
+        this.quantity = quantity;
+        this.name = name;
+        this.storeIDs = storeIDs;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Seller getSeller(Seller seller) {
+        return seller;
+    }
+
+    public void displayProductPage() {
+        System.out.println("The quantiy is: ", quantity);
+        System.out.println("The description is: ", description);
+        System.out.println("Previously purchased products are: ", cls.productsPurchased);
+    }
 }
