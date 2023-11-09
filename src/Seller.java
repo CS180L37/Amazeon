7f0a2f1d7e41e117017da81be464be973eb1de4a
Aditya Gandhi <adityagandhi98101@gmail.com>
2023-11-08 21:58:54 -0500

    updated seller.java

--- src/Seller.java
+++ src/Seller.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Seller {
+    private static String name;
+    private static int ID;
+    private static ArrayList<Product> productsSold;
+    private static ArrayList<Sale> sales;
+    public static void displayProducts() {
+        for (int i = 0; i < productsSold.size(); i++) {
+            System.out.printf("%d. %s\n", i, productsSold.get(i));
+        }
+    }
+
+    public static void displayDashboard() {
+        System.out.println("""
+How do you want to sort?
+1. Customer name
+2. Product name
+3. 
+""");
+        for (Sale sale : sales) {
+
+        }
+    }
+
+
 }
