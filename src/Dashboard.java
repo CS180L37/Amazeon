34de6a312f07047f98d906e8697c16d719f01794
ShlokSheth13 <sheth53@purdue.edu>
2023-11-12 10:55:25 -0500

    .\Commit customerMarket

--- src/Dashboard.java
+++ src/Dashboard.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 // Parent class for SellerDashboard and CustomerDashboard
-public class Dashboard<T, U> {
+public abstract class Dashboard<T, U> {
     private ArrayList<T> data1; // The data contained in the dashboard
     private ArrayList<U> data2; // The data contained in the dashboard
 
@@ -30,4 +30,12 @@ public class Dashboard<T, U> {
     public void setData2(ArrayList<U> data2) {
         this.data2 = data2;
     }
+
+    // 2 sort methods exist to account for seller having to sort two different data
+    // types
+    // Sort stores by products sold
+    public abstract ArrayList<Store> sort1(User user);
+
+    // Sort stores by products purchased by a customer
+    public abstract ArrayList<Store> sort2(User user);
 }
