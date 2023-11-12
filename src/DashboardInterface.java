0bfd0ab823f67ad0fcc6891ec578dab91decf8cb
ShlokSheth13 <sheth53@purdue.edu>
2023-11-12 14:11:36 -0500

    .\Commit customerMarket

--- src/DashboardInterface.java
+++ src/DashboardInterface.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public interface DashboardInterface<T, U> {
-    ArrayList<T> sort1(User user); // Returns a sorted list of the desired values
+    ArrayList<T> sort1(User user); // Returns a sorted list of the desired values.
 
-    ArrayList<U> sort2(User user); // Returns a sorted list of the desired values
+    ArrayList<U> sort2(User user); // Returns a sorted list of the desired values.
 
-    void displayDashboard(User user); // Display the dashboard
+    void displayDashboard(User user); // Displays the dashboard.
 }
