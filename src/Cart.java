636291a7771fe2e5c5ade1bd59e96ff235548fe3
ShlokSheth13 <sheth53@purdue.edu>
2023-11-13 15:03:09 -0500

    commit changes

--- src/Cart.java
+++ src/Cart.java
@@ -29,13 +29,13 @@ public class Cart {
     }
 
     // Adds the product to cartProducts
-    public boolean addToCart(Product product) {
-        return cartProducts.add(product);
-    }
+    public boolean addToCart (Product product) {
+        return cart Products.add(product);
+     }
 
-    // Removes the product from cartProducts
-    public boolean removeFromCart(Product product) {
-        return cartProducts.remove(product);
+    // Removes the product from cart Products
+    public boolean removeFromCart (Product product) {
+        return cartProducts.remove (product);
     }
 
     // Purchases all the products in the cart for the specific customer
@@ -95,10 +95,10 @@ public class Cart {
     }
 
     @Override
-    public String toString() {
-        return "Cart{" +
-                "customerID=" + customerID +
-                ", cartProducts=" + cartProducts +
-                '}';
-    }
+    public String to String() {
+        return "Cart {" +
+                "customerID =" + customerID +
+                ", cartProducts =" + cartProducts +
+                '} ';
+     }
 }
