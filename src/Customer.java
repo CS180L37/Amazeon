4c25bd92127e1880a18af9e86762e2e221b5238a
ShlokSheth13 <sheth53@purdue.edu>
2023-11-13 13:13:59 -0500

    commit\ Amazeon

--- src/Customer.java
+++ src/Customer.java
@@ -134,7 +134,7 @@ public class Customer extends User implements UserInterface<Customer> {
     @Override
     public String toString() {
         return "Customer {" +
-                "cart= " + cart +
-                '} ';
+                "cart=" + cart +
+                '}';
     }
 }
