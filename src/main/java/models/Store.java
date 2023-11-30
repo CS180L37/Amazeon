91a1eaa866c4b44d23287fd0422f850deadbbc3e
Aditya Gandhi <adityagandhi98101@gmail.com>
2023-11-30 18:44:17 -0500

    store.java has been made firestore-accessible

--- src/main/java/models/Store.java
+++ src/main/java/models/Store.java
@@ -1,24 +1,22 @@
 package models;
 
-import java.util.ArrayList;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import utils.Utils;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+
+import static utils.Utils.db;
 
 public class Store {
     private int storeId;
     private String name;
     private ArrayList<Product> products;
     private ArrayList<Customer> customers;
+    private static CollectionReference storeCollection = db.collection("stores");
 
-    private Store(int storeId, String name, ArrayList<Product> products, ArrayList<Customer> customers) {
-        this.name = name;
-        this.products = products;
-        this.storeId = storeId;
-        this.customers = customers;
-    }
-
-    // TODO: alternative constructor
-    public static Store createStore() {
-        return null;
-    }
 
     public String getName() {
         return this.name;
@@ -38,6 +36,7 @@ public class Store {
 
     public void setName(String name) {
         this.name = name;
+
     }
 
     public void setStoreId(int id) {
@@ -52,32 +51,53 @@ public class Store {
         this.customers = customers;
     }
 
-//    public static Store getStoreById(int id) {
-//        Store store = null;
-//        for (Store currentStore : Amazeon.stores) {
-//            if (currentStore.getStoreId() == id) {
-//                store = currentStore;
-//            }
-//        }
-//        return store;
-//    }
 
-//    public static ArrayList<Store> getStoresByIds(List<Integer> storeIds) {
-//        ArrayList<Store> stores = new ArrayList<Store>();
-////        for (int id : storeIds) {
-////            stores.add(getStoreById(id));
-////        }
-//        return stores;
-//    }
+    private Store(QueryDocumentSnapshot document) throws IOException {
+//        int x = Integer.parseInt(String.valueOf(document.getLong("customerId")));
+        int id = document.getLong("storeId").intValue();
+        this.name = document.getString("name");
+        List<Integer> productIds = (List<Integer>) document.getData().get("productIds");
+        this.products = Product.getProductsByIds((productIds != null) ? productIds : Arrays.asList());
+        this.storeId = id;
+        List<Integer> customerIds = (List<Integer>) document.getData().get("customerIds");
+        this.customers = Customer.getCustomersByIds(customerIds);
+    }
+    private Store(int cartId, ArrayList<Product> products) {
+        this.storeId = cartId;
+        this.products = products;
+    }
+//
+//    // TODO: alternative constructor
+    public static Store createStore(int cartId, ArrayList<Product> products) throws ExecutionException, InterruptedException {
+        // Add document data with auto-generated id.
+        Map<String, Object> data = new HashMap<>();
+        data.put("cartId", "id");
+        data.put("productIds", Arrays.asList());
+        ApiFuture<DocumentReference> addedDocRef = db.collection("carts").add(data);
+        System.out.println("Added document with ID: " + addedDocRef.get().getId());
+        return new Store(cartId, products);
+    }
 
-    public static int getNextStoreId() {
-        // Store store = null;
-        // for (Store currentStore : Amazeon.stores) {
-        // if (currentStore.getId() == id) {
-        // store = currentStore;
-        // }
-        // }
-        // return store;
-        return 0;
+
+    public static Store getStoreById(int givenStoreId) throws IOException {
+        ApiFuture<QuerySnapshot> future = storeCollection.select("customerId")
+                .where(Filter.equalTo("customerId", givenStoreId)).limit(1).get();
+        List<QueryDocumentSnapshot> documents = Utils.retrieveData(future);
+        return new Store(documents.get(0));
+    }
+
+    public static ArrayList<Store> getStoresByIds(List<Integer> storeIds) throws IOException {
+        ArrayList<Store> stores = new ArrayList<Store>();
+        for (int id : storeIds) {
+            stores.add(getStoreById(id));
+        }
+        return stores;
+    }
+
+    public static int getNextStoreId() throws IOException {
+        ApiFuture<QuerySnapshot> future = storeCollection.orderBy("storeId", Query.Direction.DESCENDING)
+                .limit(1).get();
+        List<QueryDocumentSnapshot> documents = Utils.retrieveData(future);
+        return documents.get(0).getLong("storeId").intValue() + 1;
     }
 }
