e7aa5f1bece4b57ed26cb42d8f14ff4af139b971
xanmankey <86136010+xanmankey@users.noreply.github.com>
2023-11-30 19:34:45 -0500

    commented out integration test

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,17 +1,17 @@
 
 /// Test the entire app, all in one
-import org.junit.Test;
+// import org.junit.Test;
 
-public class IntegrationTest extends TestUtils {
-    @Test
-    public void testSeller() {
-        // System.setIn(new ByteArrayInputStream(
-        // ""
-        // .getBytes()));
-    }
+// public class IntegrationTest extends TestUtils {
+// @Test
+// public void testSeller() {
+// // System.setIn(new ByteArrayInputStream(
+// // ""
+// // .getBytes()));
+// }
 
-    @Test
-    public void testCustomer() {
+// @Test
+// public void testCustomer() {
 
-    }
-}
+// }
+// }
